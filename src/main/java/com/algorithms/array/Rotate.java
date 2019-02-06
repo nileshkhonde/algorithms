135ccd67156670cd8fb8799067e9d1a759b64b3b
Nilesh Khonde <47116910+nileshkhonde@users.noreply.github.com>
2019-02-05 22:59:37 -0500

    Create Rotate.java

---
+++ src/main/java/com/algorithms/array/Rotate.java
@@ -0,0 +1,14 @@
+package com.algorithms.array;
+
+public class Rotate {
+	public void rotate(int[] nums, int k) {
+        
+        for(int i=0; i<k;i++){
+            int temp = nums[nums.length-1];
+             for(int j=nums.length-1;j>0;j--){
+                 nums[j]=nums[j-1];
+             }
+            nums[0]=temp;
+        }
+    }
+}
