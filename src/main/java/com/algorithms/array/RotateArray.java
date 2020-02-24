766dbc8832daf3997466922b955f554fd5cb4eca
Nilesh Khonde <47116910+nileshkhonde@users.noreply.github.com>
2020-02-23 19:07:30 -0500

    Create RotateArray.java

---
+++ src/main/java/com/algorithms/array/RotateArray.java
@@ -0,0 +1,15 @@
+//given an n array rotate elements by k
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int temp = 0;
+        
+        for(int i=0;i<k;i++){
+            temp = nums[nums.length-1];
+            for(int j=nums.length-1;j>0;j--){
+                nums[j]=nums[j-1];
+            }
+            nums[0]=temp;
+        }
+    }
+}
