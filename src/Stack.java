d2581604b07222eb43e8ac10a5799124c733f95f
Nilesh Khonde <47116910+nileshkhonde@users.noreply.github.com>
2020-07-12 15:11:55 -0400

    Update Stack.java

--- src/Stack.java
+++ src/Stack.java
@@ -42,4 +42,16 @@ public class Stack {
 			return -1;
 		}
 	}
+	
+	public static void main(String[] args) {
+		Stack s = new Stack(10);
+
+		for(int i=0;i<12;i++) {
+			s.push(i);
+		}
+
+		for(int i=0;i<12;i++) {
+			System.out.println(s.pop());
+		}
+	}
 }
